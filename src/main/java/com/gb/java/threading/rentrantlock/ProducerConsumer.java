3f5466d9b5cb2301ce06788ca9b28b168648c1c2
Gopalakrishnan <fax6@INMACY4AAH.local>
2017-10-21 13:38:58 +0530

    producer consumer with reentrant locks

---
+++ src/main/java/com/gb/java/threading/rentrantlock/ProducerConsumer.java
@@ -0,0 +1,75 @@
+package com.gb.java.threading.rentrantlock;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by fax6 on 21/10/17.
+ */
+public class ProducerConsumer {
+    static Lock lock = new ReentrantLock(true);
+    static Condition condition = lock.newCondition();
+    static int count = 0;
+    static void produce(){
+        while (true){
+            if (count <=5){
+                lock.lock();
+                count++;
+                condition.signal();
+                System.out.println("Producing item " + count);
+            }
+            else {
+                try {
+                    condition.await();
+                    lock.unlock();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static void consume(){
+        while (true){
+            if (count >= 1){
+                lock.lock();
+                count--;
+                condition.signal();
+                System.out.println("Consuming item " + count);
+            }
+            else {
+                try {
+                    condition.await();
+                    lock.unlock();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(()->produce());
+        Thread t2 = new Thread(()->consume());
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Complete...");
+    }
+}
